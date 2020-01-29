4990b2c292012ef251ffa593112df84c39868e5a
CyTyDS <ds1997@hotmail.fr>
2020-01-29 03:32:14 +0100

    initiated project with link 'securing web'

---
+++ coffee-and-co-manager/src/main/java/com/m2gil/coffeeandcomanager/CoffeeAndCoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.m2gil.coffeeandcomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeAndCoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeAndCoManagerApplication.class, args);
+	}
+
+}
---
+++ coffee-and-co-manager/src/test/java/com/m2gil/coffeeandcomanager/CoffeeAndCoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2gil.coffeeandcomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeAndCoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
