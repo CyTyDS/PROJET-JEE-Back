1ed104bd65c1ff84a1a22f38840fa64725c72eb5
CyTyDS <ds1997@hotmail.fr>
2020-01-30 01:22:21 +0100

    changed project to use baeldung implementation security angular

--- coffee-and-co-manager/src/main/java/com/m2gil/coffeeandcomanager/WebSecurityConfig.java
+++ coffee-and-co-manager/src/main/java/com/m2gil/coffeeandcomanager/WebSecurityConfig.java
@@ -1,44 +1,64 @@
 package com.m2gil.coffeeandcomanager;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+// Using https://www.baeldung.com/spring-security-login-angular
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				// "/" and "/home" paths do not require auth
-				.antMatchers("/", "/home").permitAll()
-				.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				.loginPage("/login")
-				.permitAll()
-				.and()
-			.logout()
-				.permitAll();
-	}
-
-	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
-
-		return new InMemoryUserDetailsManager(user);
-	}
+    protected void configure(AuthenticationManagerBuilder auth)
+      throws Exception {
+        auth
+          .inMemoryAuthentication()
+          .withUser("user")
+          .password("password")
+          .roles("USER");
+    }
+ 
+    @Override
+    protected void configure(HttpSecurity http) 
+      throws Exception {
+        http.csrf().disable()
+          .authorizeRequests()
+          .antMatchers("/login").permitAll()
+          .anyRequest()
+          .authenticated()
+          .and()
+          .httpBasic();
+    }
+    
+    // Using https://spring.io/guides/gs/securing-web/
+//  @Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http
+//			.authorizeRequests()
+//				.antMatchers("/", "/home").permitAll()
+//				.anyRequest().authenticated()
+//				.and()
+//			.formLogin()
+//				.loginPage("/login")
+//				.permitAll()
+//				.and()
+//			.logout()
+//				.permitAll();
+//	}
+//
+//	@Bean
+//	@Override
+//	public UserDetailsService userDetailsService() {
+//		UserDetails user =
+//			 User.withDefaultPasswordEncoder()
+//				.username("user")
+//				.password("password")
+//				.roles("USER")
+//				.build();
+//
+//		return new InMemoryUserDetailsManager(user);
+//	}
 }
---
+++ coffee-and-co-manager/src/main/java/com/m2gil/coffeeandcomanager/credentials/User.java
@@ -0,0 +1,20 @@
+package com.m2gil.coffeeandcomanager.credentials;
+
+public class User {
+
+	private String userName;
+	private String password;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
